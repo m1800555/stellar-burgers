dcf8a8a0760fbf0193ddfd6417233c84e59ece2b
Anton Kuzmenko <anton.kuzmenko2014@mail.ru>
2025-11-20 21:20:24 +0300

    Step 1: Add routing

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,76 @@
-import { ConstructorPage } from '@pages';
+import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
+
 import '../../index.css';
 import styles from './app.module.css';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
+import { AppHeader, IngredientDetails, OrderInfo, Modal } from '@components';
 
-import { AppHeader } from '@components';
+const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
-);
+  const background = location.state?.background;
+
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <main>
+        <Routes location={background || location}>
+          <Route path='*' element={<NotFound404 />} />
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/feed/:number' element={<OrderInfo />} />
+          <Route path='/ingredients/:id' element={<IngredientDetails />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='/profile/orders/:number' element={<OrderInfo />} />
+        </Routes>
+
+        {background && (
+          <Routes>
+            <Route
+              path='/feed/:number'
+              element={
+                <Modal title='Информация о заказе' onClose={() => navigate('/feed')}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+            <Route
+              path='/ingredients/:id'
+              element={
+                <Modal title='Детали ингредиента' onClose={() => navigate('/')}>
+                  <IngredientDetails />
+                </Modal>
+              }
+            />
+            <Route
+              path='/profile/orders/:number'
+              element={
+                <Modal title='Информация о заказе' onClose={() => navigate('/profile/orders')}>
+                  <OrderInfo />
+                </Modal>
+              }
+            />
+          </Routes>
+        )}
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as ReactDOMClient from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './components/app/app';
 
 const container = document.getElementById('root') as HTMLElement;
@@ -7,6 +9,8 @@ const root = ReactDOMClient.createRoot(container!);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
